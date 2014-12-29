1ba86f08ab46bfb8a40c7aa696a823172e7b526d
McStrain <cbrandst@gmail.com>
2014-12-29 09:51:56 -0600

    Page 284

---
+++ PseudoCode/Assets/MyExample.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyExample : MonoBehaviour {
+	// Use this for initialization
+	void Start () {
+		MyMembers m = new MyMembers();
+		m.FirstFunction();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bool AKey = Input.GetKey (KeyCode.A);
+		Debug.Log (AKey);
+
+	
+	}
+}

67b50118ce76dbae224de02012e877e99e17c213
McStrain <cbrandst@gmail.com>
2014-12-30 10:37:24 -0600

    Page 294

---
+++ ZombieConstructor/Assets/MyZombie.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyZombie : MonoBehaviour {
+	public string Name;
+	public int brainsEaten;
+	public int hitPoints;
+	public MyZombie()
+	{
+		Name = "MyZombie";
+		brainsEaten = 0;
+		hitPoints = 10;
+	}
+
+	MyZombie rob = new MyZombie();
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

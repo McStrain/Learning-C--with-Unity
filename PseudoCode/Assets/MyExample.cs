9736839250998e50349dd6123312df93f9811e62
McStrain <cbrandst@gmail.com>
2014-12-29 20:11:53 -0600

    Page 288
    Bottom of page

--- PseudoCode/Assets/MyExample.cs
+++ PseudoCode/Assets/MyExample.cs
@@ -2,17 +2,37 @@
 using System.Collections;
 
 public class MyExample : MonoBehaviour {
+	public Vector3 pos;
 	// Use this for initialization
 	void Start () {
-		MyMembers m = new MyMembers();
-		m.FirstFunction();
+		//MyMembers m = new MyMembers();
+		//m.FirstFunction();
+		pos = transform.position;
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		bool AKey = Input.GetKey (KeyCode.A);
-		Debug.Log (AKey);
-
+		bool SKey = Input.GetKey (KeyCode.S);
+		bool WKey = Input.GetKey (KeyCode.W);
+		bool DKey = Input.GetKey (KeyCode.D);
+		//Debug.Log (AKey);
+		if (AKey) {
+			pos.x = pos.x - 0.1f;
+			print ("A was pressed");
+		}
+		if (SKey) {
+			pos.z = pos.z - 0.1f;
+			print ("S was pressed");
+		}
+		if (WKey) {
+			pos.z = pos.z + 0.1f;
+			print ("W was pressed");
+		}
+		if (DKey) {
+			pos.x = pos.x + 0.1f;
+			print ("D was pressed");
+		}
+		transform.position = pos;
 	
 	}
 }
---
+++ PseudoCode/Assets/MyExample2.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyExample2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		print (NumbersAreTheSame(3, 3));
+		print (NumbersAreTheSame(4, 3));
+	}
+
+
+	bool NumbersAreTheSame(int a, int b) {
+		if (a == b) {
+			return true;
+		}
+		return false;
+	}
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

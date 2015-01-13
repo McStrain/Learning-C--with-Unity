faf2b9d25f44f338964d2b5ddaed733b595e37c0
McStrain <cbrandst@gmail.com>
2015-01-13 08:32:32 -0600

    Page 378
    After code in basic example

---
+++ MyGotoLabels/Assets/Examples.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Examples : MonoBehaviour {
+	public int num = 1;
+	// Use this for initialization
+	void Start () {
+		if (num > 0);
+		{
+			goto MyLabel;  // goes to MyLabel
+		}
+		print ("number was less than 0");  // gets skipped if num isn't > 0
+	MyLabel:  // goes here when num is greater than 0
+			print ("I jumped to MyLabel");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

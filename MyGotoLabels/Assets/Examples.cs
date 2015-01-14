b3f5626e4149edaa95e02751b22ab804e3ab16f6
McStrain <cbrandst@gmail.com>
2015-01-14 08:33:17 -0600

    Page 378 - finally through the goto section.

--- MyGotoLabels/Assets/Examples.cs
+++ MyGotoLabels/Assets/Examples.cs
@@ -5,13 +5,25 @@ public class Examples : MonoBehaviour {
 	public int num = 1;
 	// Use this for initialization
 	void Start () {
-		if (num > 0);
+		if (num > 0)
 		{
+			print ("just inside if");
 			goto MyLabel;  // goes to MyLabel
 		}
 		print ("number was less than 0");  // gets skipped if num isn't > 0
 	MyLabel:  // goes here when num is greater than 0
-			print ("I jumped to MyLabel");
+		print ("I jumped to MyLabel");
+/*
+		if (true) 
+		{
+			print ("This is true so it prints.");
+		}
+
+		if (false) 
+		{
+			print ("This is false so it shouldn't print.");
+		}
+*/
 	}
 	
 	// Update is called once per frame

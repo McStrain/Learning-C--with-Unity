aeb4d21edde740590af43762547205e05e55ea85
McStrain <cbrandst@gmail.com>
2015-01-17 12:24:22 -0600

    Page 391
    Not sure where I need to add the chunk of code.

---
+++ MyGotoLabels/Assets/HumanState.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanState : ZombieState {
+
+	override public void LookAround()
+	{
+		GameObject[] Zombies = (GameObject[]) GameObject.FindObjectsOfType(typeof(GameObject));
+		foreach (GameObject go in Zombies) 
+		{
+			ZombieState z = go.GetComponent<ZombieState>();
+			if(z == null || z == this)
+			{
+				print ("Entered the null loop.  Continue is next statement.");
+				continue;
+			}
+			print ("This is right after the continue/if loop");
+			Vector3 v = go.transform.position - transform.position;
+			float distanceToGo = v.magnitude;
+			if (distanceToGo < closestDistance)
+			{
+				if (z is ZombieState)
+				{
+					closestDistance = distanceToGo;
+					closestGameObject = go;
+				}
+			}
+			if (distanceToGo > furthestDistance)
+			{
+				if (z is HumanState)
+				{
+					furthestDistance = distanceToGo;
+					furthestGameObject = go;
+				}
+			}
+		}
+	}
+}
--- MyGotoLabels/Assets/ZombieState.cs
+++ MyGotoLabels/Assets/ZombieState.cs
@@ -10,10 +10,10 @@ public class ZombieState : MonoBehaviour {
 	ZState MyState;
 
 	float stateTimer;
-	float closestDistance;
-	float furthestDistance;
-	GameObject closestGameObject;
-	GameObject furthestGameObject;
+	public float closestDistance;
+	public float furthestDistance;
+	public GameObject closestGameObject;
+	public GameObject furthestGameObject;
 	// Use this for initialization
 	void Start () {
 		stateTimer = 0.1f;
@@ -35,13 +35,17 @@ public class ZombieState : MonoBehaviour {
 	Ideling:
 			stateTimer -= Time.deltaTime;
 			if(stateTimer < 0.0f)
-		{
+		{	
 			MyState = ZState.wandering;
 			stateTimer = 3.0f;
+			closestDistance = Mathf.Infinity;
+			furthestDistance = 0f;
+			LookAround ();
 		}
 			return;
 	Wandering:
 			stateTimer -= Time.deltaTime;
+			MoveAround ();
 		if(stateTimer < 0.0f)
 		{
 			MyState = ZState.idleing;
@@ -51,7 +55,7 @@ public class ZombieState : MonoBehaviour {
 
 	}
 
-	void LookAround()
+	virtual public void LookAround()
 	{
 		GameObject[] Zombies = (GameObject[]) GameObject.FindObjectsOfType(typeof(GameObject));
 		foreach (GameObject go in Zombies) 
@@ -59,8 +63,10 @@ public class ZombieState : MonoBehaviour {
 			ZombieState z = go.GetComponent<ZombieState>();
 			if(z == null || z == this)
 			{
+				print ("Entered the null loop.  Continue is next statement.");
 				continue;
 			}
+			print ("This is right after the continue/if loop");
 			Vector3 v = go.transform.position - transform.position;
 			float distanceToGo = v.magnitude;
 			if (distanceToGo < closestDistance)
@@ -75,4 +81,18 @@ public class ZombieState : MonoBehaviour {
 			}
 		}
 	}
+
+	void MoveAround()
+	{
+		Vector3 MoveAway = (transform.position - closestGameObject.transform.position).normalized;
+		Vector3 MoveTo = (transform.position - furthestGameObject.transform.position).normalized;
+		Vector3 directionToMove = MoveAway - MoveTo;
+		transform.forward = directionToMove;
+//		gameObject.rigidbody.velocity = directionToMove * Random.Range (10, 30) * 0.1f;
+		gameObject.rigidbody.velocity = directionToMove * 0.3f;
+		Debug.DrawRay (transform.position, directionToMove, Color.blue);
+		Debug.DrawRay(transform.position,closestGameObject.transform.position, Color.red);
+		Debug.DrawLine(transform.position, furthestGameObject.transform.position, Color.grey);
+	}
+
 }

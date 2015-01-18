56b7eac3167ca35d8b8993e1c4d47146b16396aa
McStrain <cbrandst@gmail.com>
2015-01-18 15:03:23 -0600

    Page 400

--- MyMoreArrays/Assets/Examples.cs
+++ MyMoreArrays/Assets/Examples.cs
@@ -2,43 +2,20 @@
 using System.Collections;
 
 public class Examples : MonoBehaviour {
-
-
 	// Use this for initialization
 	void Start () {
-		int[] primes = {1,3,5,7,11,13,17,23,27,31};
-		int[] fibonacci = {0,1,1,2,3,5,8,13,21,34,55,89,144};
-		int[] powersOfTwo = {1,2,4,8,16,32,64,128,256,512,1024};
 
-		ArrayList Numbers = new ArrayList{primes, fibonacci, powersOfTwo};
-		object[] thing = Numbers.ToArray() as object[];
-		print (thing.Length);
+	}
+
+	void Update()
+	{
+		GameObject go = GameObject.Find("Cube");
+		Vector3 CubePosition = go.transform.position;
+		Vector3 Up = new Vector3(0, 10, 0);
+		Debug.DrawRay (CubePosition, Up);
 
 
-/*		int numArrays = Numbers.Count;
 
-		foreach (int[] Nums in Numbers)
-		{
-			foreach(int n in Nums)
-			{
-				print (n);
-			}
-		}
-*/
-	//	int items = primes.Length;
-
-	//	for (int i = 0; i < items; i++) 
-	//	{
-	//		print (primes[i]);
-	//	}
-
-	//	int j = 0;
-	//	while (j < items)
-	//	{
-	//		print (primes [j]);
-	//		j++;
-	//	}
-	
 	}
 	
 

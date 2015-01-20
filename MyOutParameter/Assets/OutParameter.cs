4c478ed1d5c0cf8a52f9f23b53c44395e83cda93
McStrain <cbrandst@gmail.com>
2015-01-19 18:45:45 -0600

    * Page 408.  Stepped through the first code block.  Ready to move on.

--- MyOutParameter/Assets/OutParameter.cs
+++ MyOutParameter/Assets/OutParameter.cs
@@ -2,9 +2,21 @@
 using System.Collections;
 
 public class OutParameter : MonoBehaviour {
-
+	public GameObject[] GameObjectArray;
 	// Use this for initialization
-	void Start () {
+	void Start () 
+	{
+		ArrayList aList = new ArrayList();
+		GameObject[] gameObjects = (GameObject[])GameObject.FindObjectsOfType(typeof(GameObject));
+		foreach (GameObject go in gameObjects)
+		{
+			if(go.name == "Sphere")
+			{
+				aList.Add (go);
+			}
+		}
+		GameObjectArray = aList.ToArray (typeof(GameObject)) as GameObject[];
+
 	
 	}
 	

edf2895209aae88e872fd4c160e2bfd6d4c1467c
McStrain <cbrandst@gmail.com>
2015-01-20 09:33:18 -0600

    Page 417, Type Casting Numbers

---
+++ Page-413/Assets/Example.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+
+public class Example : MonoBehaviour {
+	void someFunction(out int x, out int y)
+	{
+		x = 7;
+		y = 13;
+	}
+	void Start()
+	{
+		int firstInt = 0;
+		int secondInt = 0;
+		someFunction(out firstInt, out secondInt);
+		print (firstInt + " " + secondInt);
+	}
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

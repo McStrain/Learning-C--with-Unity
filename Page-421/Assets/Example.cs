7f44b00172b92dcaa56d9bfd320f5c5a1d7ea303
McStrain <cbrandst@gmail.com>
2015-01-20 21:10:32 -0600

    Page 424 - top paragraph

---
+++ Page-421/Assets/Example.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+	{
+		int a = GameObject.CreatePrimitive(PrimitiveType.Capsule).GetInstanceID();
+		int b = a;
+		print (a);
+		print (b);
+		print (a == b);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}

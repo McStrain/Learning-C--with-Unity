23282e9dc45b7a1724f619e7fe05d8ae5bebf373
McStrain <cbrandst@gmail.com>
2015-01-21 09:03:53 -0600

    Page 426
    * Page 426 - Operator Overloading.  Started the Zombie adding to
    another Zombie example which is failing.  Next up is to Overload so I
    can add what I want.  Have to tell the code what it is I’m wanting to
    add as you can’t just add an object to an object.  And this is
    overloading.

---
+++ Page-421/Assets/Zombie.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Zombie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Zombie a = new Zombie();
+		Zombie b = new Zombie();
+		Zombie c = a + b;
+		print ("test");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

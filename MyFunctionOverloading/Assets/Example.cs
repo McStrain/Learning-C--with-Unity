0ab3dc0741f103c64291995f82761e793830cad0
McStrain <cbrandst@gmail.com>
2015-01-24 13:00:16 -0600

    Page 450 7.4.3

---
+++ MyFunctionOverloading/Assets/Example.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example : MonoBehaviour {
+	int RetVal()
+	{
+		return 1;
+	}
+	float RetVal()
+	{
+		return 1.0f;
+	}
+
+	// Use this for initialization
+	void Start () 
+	{
+		int a = RetVal ();
+		float b = RetVal ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

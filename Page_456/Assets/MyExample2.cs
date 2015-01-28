5bbd1a77feaaf946f1d1dc369eebe12557cce021
McStrain <cbrandst@gmail.com>
2015-01-27 22:28:38 -0600

    * Page 458 - Entered first code block but haven't ran it or understood it yet.

---
+++ Page_456/Assets/MyExample2.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyExample2 : MonoBehaviour {
+	TestClass myTestClass = new TestClass();
+	// Use this for initialization
+	void Start () 
+	{
+		myTestClass.Name = "John";		
+		print (myTestClass.Name);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+public class TestClass
+	{
+		private string name;
+		public string Name
+		{
+			get
+			{
+				return name;
+			}
+			set
+			{
+				name = value;
+			}
+		}
+	}
+}
---
+++ Page_456/Assets/Page_458.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Page_458 : MonoBehaviour {
+	class GetSet
+	{
+		public delegate void MyIntHandler(int i);
+		public event MyIntHandler MyIntEvent;
+		private int myInt;
+		public int MyInt
+		{
+			get {return myInt;}
+			set
+			{
+				myInt = value;
+				//raise an event if there are any assigned functions
+				if (MyIntEvent != null)
+				{
+					MyIntEvent(myInt);
+				}
+			}
+		}
+	}
+
+
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		GetSet gs = new GetSet();
+		//assign a function to be raised when the falue is changed
+		gs.MyIntEvent += IntChanged;
+		//set a value, this will raise an event!
+		gs.MyInt = 10;
+	}
+
+	void IntChanged (int i)
+	{
+		print ("change! " + i);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

ae19e72af7c4d826cb7cae54fe52f19f48a792ec
McStrain <cbrandst@gmail.com>
2015-02-03 09:13:09 -0600

    Page 497.
    After first code block

---
+++ ClassConstructorsAgain/Assets/MyExample.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyExample : MonoBehaviour {
+	//sub class declaration
+	public class MyClass
+	{
+		//sub-class string called name
+		private string name;
+		//funciton with same identifier as class name
+		public MyClass(string n)
+		{
+			//assigns the internal name to string n
+			name = n;
+		}
+
+		public MyClass(int n)
+		{
+			name = n.ToString ();
+		}
+	}
+	// Use this for initialization
+	void Start () 
+	{
+	//uses the class constructor, names the class bob
+		MyClass mc = new MyClass("bob");
+		MyClass mc2 = new MyClass(3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ClassConstructorsAgain/Assets/MyTreadmillManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyTreadmillManager : MonoBehaviour {
+	class Obstacle
+	{
+		GameObject obstacle;
+	}
+	private Obstacle[] obstacles;
+	public int ObstacleCount = 10;
+
+	// Use this for initialization
+	void Start () 
+	{
+		obstacles = new Obstacle[ObstacleCount];
+		//creates a new array of the size requested
+		for (int i = 0; i < ObstacleCount; i++)
+		{
+			obstacles [i] = new Obstacle();
+			//fills in the array with new obstacles
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
